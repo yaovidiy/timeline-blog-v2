147f660d9e99e9601eab3ffe8afe032b5e397f42
Yaroslav Ovdii <yaovdiy@gmail.com>
2024-05-02 10:02:46 +0300

    prepared setup for user login

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -10,16 +10,27 @@ const client = new PrismaClient();
 const adapter = new PrismaAdapter(client.session, client.user);
 
 export const lucia = new Lucia(adapter, {
-	sessionCookie: {
-		attributes: {
-			// set to `true` when using HTTPS
-			secure: !dev
-		}
-	}
+  sessionCookie: {
+    attributes: {
+      // set to `true` when using HTTPS
+      secure: !dev
+    }
+  },
+  getUserAttributes: (attributes) => {
+    return {
+      // attributes has the type of DatabaseUserAttributes
+      username: attributes.username
+    };
+  }
 });
 
 declare module "lucia" {
-	interface Register {
-		Lucia: typeof lucia;
-	}
+  interface Register {
+    Lucia: typeof lucia;
+    DatabaseUserAttributes: DatabaseUserAttributes;
+  }
+}
+
+interface DatabaseUserAttributes {
+  username: string;
 }
